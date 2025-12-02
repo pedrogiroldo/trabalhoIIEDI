8feebfe9972f16672c8e2cb84da4dc22024f9935
Pedro Andrade Giroldo <pedrogiroldo40@gmail.com>
2025-12-02 18:41:33 -0300

    feat: implement basic query command parsing and execution framework with `QryResults` type.

--- src/lib/qry_handler/qry_handler.c
+++ src/lib/qry_handler/qry_handler.c
@@ -1,1 +1,29 @@
 #include "qry_handler.h"
+#include "../commons/queue/queue.h"
+#include "../file_reader/file_reader.h"
+#include <string.h>
+
+typedef struct {
+
+} QryResults_t;
+
+// private functions defined as static and implemented
+// on the end of the file
+static void execute_a_command();
+
+void execute_qry_commands(FileData *file_data, char *output_path) {
+  Queue file_lines_queue = get_file_lines_queue(file_data);
+  while (!queue_is_empty(file_lines_queue)) {
+    char *line = (char *)queue_dequeue(file_lines_queue);
+    char *command = strtok(line, " ");
+
+    if (strcmp(command, "a") == 0) {
+      execute_a_command();
+    } else if (strcmp(command, "d") == 0) {
+    } else if (strcmp(command, "p") == 0) {
+    } else if (strcmp(command, "cln") == 0) {
+    }
+  }
+}
+
+static void execute_a_command() {}
--- src/lib/qry_handler/qry_handler.h
+++ src/lib/qry_handler/qry_handler.h
@@ -1,6 +1,6 @@
 #ifndef QRY_HANDLER_H
 #define QRY_HANDLER_H
 
-
+typedef void *QryResults;
 
 #endif // QRY_HANDLER_H
